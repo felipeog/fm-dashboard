fa333d146ce0a6c4dfc47306c2c76941cec7e004
felipeog <felipeog476@gmail.com>
2021-02-14 14:20:50 -0300

    Add theme toggle

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
-console.log('Hello')
+const htmlElement = document.querySelector('html')
+const themeToggle = document.querySelector('.toggle')
+
+const toggleTheme = () => {
+  const currentTheme = htmlElement.getAttribute('data-theme')
+  const nextTheme = currentTheme === 'dark' ? 'light' : 'dark'
+
+  htmlElement.setAttribute('data-theme', nextTheme)
+}
+
+const init = () => {
+  themeToggle.addEventListener('click', toggleTheme)
+}
+
+window.addEventListener('load', init)
